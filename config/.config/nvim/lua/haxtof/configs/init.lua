4fc1b89faae0782776999a9de281d65c510fd5d1
Flavio Milan <oflaviomilan@gmail.com>
2024-03-05 22:43:23 -0300

    imp: adding java lsp

--- config/.config/nvim/lua/haxtof/configs/init.lua
+++ config/.config/nvim/lua/haxtof/configs/init.lua
@@ -1,6 +1,7 @@
 -- Assuming each of your plugin config files returns a table of plugin specs
 local configs = {
   require 'haxtof.configs.obsidian',
+  require 'haxtof.configs.java',
 }
 
 -- Return the aggregated plugin specifications
