52ad99d5bf9249b5c1fe1d26bdaea5d7bb31c739
Flavio Milan <oflaviomilan@gmail.com>
2025-01-31 19:08:07 -0300

    config nvim

--- configs/nvim/.config/nvim/lua/milan/init.lua
+++ configs/nvim/.config/nvim/lua/milan/init.lua
@@ -1,2 +1,48 @@
+require("milan.set")
 require("milan.remap")
-require("milan.lazy")
+require("milan.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local milanGroup = augroup('milanGroup', {})
+local yankGroup = augroup('yankGroup', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('TextYankPost', {
+    group = yankGroup,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 60,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = milanGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+    group = ThePrimeagenGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
